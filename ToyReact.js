92b3cbe49655cca48b0a7911d215892fdc4593e1
fan-zhang <fan.zhang@neox-inc.com>
2020-08-02 22:48:12 +0800

    dom比较

--- ToyReact.js
+++ ToyReact.js
@@ -1,31 +1,61 @@
 class ElementWrapper {
     // 对于不是原生type 的dom做包装
     constructor(type) {
-        this.root = document.createElement(type)
+        this.type = type;
+        this.props = Object.create(null);
+        this.children = [];
     }
     setAttribute(name, value) {
-        if(name.match(/^on([\s\S]+)$/)) {
-            let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase());
-            this.root.addEventListener(eventName, value);
-        }
-        if(name === 'className')
-            name = 'class';
-        this.root.setAttribute(name, value);
+        // if(name.match(/^on([\s\S]+)$/)) {
+        //     let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase());
+        //     this.root.addEventListener(eventName, value);
+        // }
+        // if(name === 'className')
+        //     name = 'class';
+        // this.root.setAttribute(name, value);
+        this.props[name] = value;
     }
     appendChild(vchild) {
-        let range = document.createRange();
-        if(this.root.children.length) {
-            range.setStartAfter(this.root.lastChild);
-            range.setEndAfter(this.root.lastChild);
-        } else {
-            range.setStart(this.root, 0);
-            range.setEnd(this.root, 0);
-        }
-        vchild.mountTo(range)
+        this.children.push(vchild);
+        // let range = document.createRange();
+        // if(this.root.children.length) {
+        //     range.setStartAfter(this.root.lastChild);
+        //     range.setEndAfter(this.root.lastChild);
+        // } else {
+        //     range.setStart(this.root, 0);
+        //     range.setEnd(this.root, 0);
+        // }
+        // vchild.mountTo(range)
     }
     mountTo(range) {
         range.deleteContents();
-        range.insertNode(this.root)
+        let element = document.createElement(this.type);
+
+
+        for (let name in this.props) {
+            let value = this.props[name];
+            if (name.match(/^on([\s\S]+)$/)) {
+                let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase());
+                element.addEventListener(eventName, value);
+            }
+            if (name === 'className')
+                name = 'class';
+            element.setAttribute(name, value);
+        }
+
+        for (let child of this.children) {
+            let range = document.createRange();
+            if (element.children.length) {
+                range.setStartAfter(element.lastChild);
+                range.setEndAfter(element.lastChild);
+            } else {
+                range.setStart(element, 0);
+                range.setEnd(element, 0);
+            }
+            child.mountTo(range)
+        }
+
+        range.insertNode(element)
     }
 }
 class TextWrapper {
@@ -44,7 +74,7 @@ export class Component {
         this.props = Object.create(null);
     }
     setAttribute(name, value) {
-        if(name.match(/^on([\s\S]+)$/)) {
+        if (name.match(/^on([\s\S]+)$/)) {
             console.log(RegExp.$1)
         }
         this.props[name] = value;
@@ -73,10 +103,10 @@ export class Component {
     }
     setState(state) {
         let merge = (oldState, newState) => {
-            for (let p  in newState) {
-                if(typeof newState[p] === 'object' && newState[p] !== null) {
-                    if(typeof oldState[p] !== 'object') {
-                        if(newState[p] instanceof Array)
+            for (let p in newState) {
+                if (typeof newState[p] === 'object' && newState[p] !== null) {
+                    if (typeof oldState[p] !== 'object') {
+                        if (newState[p] instanceof Array)
                             oldState[p] = []
                         else
                             oldState[p] = {}
@@ -87,7 +117,7 @@ export class Component {
                 }
             }
         }
-        if(!this.state && state)
+        if (!this.state && state)
             this.state = {};
         merge(this.state, state)
         this.update();
@@ -97,28 +127,28 @@ export let ToyReact = {
     // 读取到
     createElement(type, attributes, ...children) {
         let element;
-        if(typeof type === 'string')
+        if (typeof type === 'string')
             element = new ElementWrapper(type);
-        else 
+        else
             element = new type;
 
-        for(let name in attributes) {
+        for (let name in attributes) {
             element.setAttribute(name, attributes[name]);
         }
 
         let insertChildren = (children) => {
             for (let child of children) {
-                if(typeof child === 'object' && child instanceof Array) {
+                if (typeof child === 'object' && child instanceof Array) {
                     insertChildren(child);
                 } else {
-                    if(child === null || child === void 0)
+                    if (child === null || child === void 0)
                         child = '';
-                    if(!(child instanceof Component) 
-                        && !(child instanceof ElementWrapper) 
+                    if (!(child instanceof Component)
+                        && !(child instanceof ElementWrapper)
                         && !(child instanceof TextWrapper)) {
                         child = String(child);
                     }
-                    if( typeof child === 'string')
+                    if (typeof child === 'string')
                         child = new TextWrapper(child)
                     element.appendChild(child);
                 }
@@ -129,7 +159,7 @@ export let ToyReact = {
     },
     render(vdom, element) {
         let range = document.createRange();
-        if(element.children) {
+        if (element.children) {
             range.setStartAfter(element.lastChild);
             range.setEndAfter(element.lastChild);
         } else {
